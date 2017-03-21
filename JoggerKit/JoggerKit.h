3bfecf4fda8816fad3b1d5dd548ad10a9d19d1e4
Kamil Badyla <kamilbadyla@gmail.com>
2017-03-21 09:23:30 +0100

    Added JoggerKit

---
+++ JoggerKit/JoggerKit.h
@@ -0,0 +1,19 @@
+//
+//  JoggerKit.h
+//  JoggerKit
+//
+//  Created by Kamil Badyla on 21.03.2017.
+//  Copyright © 2017 Science Spir.IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JoggerKit.
+FOUNDATION_EXPORT double JoggerKitVersionNumber;
+
+//! Project version string for JoggerKit.
+FOUNDATION_EXPORT const unsigned char JoggerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JoggerKit/PublicHeader.h>
+
+
